b03af10d0bf5e361419bbaac6821049f6ce379d1
raldeskrisnu <raldeskrisnu@gmail.com>
2024-12-20 23:51:06 +0700

    Update License and README.md

--- main.go
+++ main.go
@@ -58,6 +58,45 @@ EXPOSE 8080
 CMD ["./main"]
 `
 
+const readmeTemplate = `# {{.ProjectName}}
+
+This is the {{.ProjectName}} project.
+`
+
+const gitignoreTemplate = `# Binaries for programs and plugins
+*.exe
+*.exe~
+*.dll
+*.so
+*.dylib
+
+# Output of the go coverage tool, specifically when used with LiteIDE
+*.out
+`
+
+const licenseTemplate = `MIT License
+
+Copyright (c) {{.Year}} {{.Author}}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+`
+
 // Main function to create the project scaffolding
 func main() {
 	if len(os.Args) < 2 {
@@ -86,6 +125,7 @@ func main() {
 			fmt.Println("Error creating directory:", path, err)
 			return
 		}
+		fmt.Println("Successfully created directory:", path)
 	}
 
 	// Create files
@@ -95,10 +135,18 @@ func main() {
 		"configs/config.yaml": configYAMLTemplate,
 		"Dockerfile":          dockerfileTemplate,
 		"Makefile":            makefileTemplate,
+		"README.md":           readmeTemplate,
+		".gitignore":          gitignoreTemplate,
+		"LICENSE":             licenseTemplate,
 	}
 	// Create the files from the templates
 	for path, content := range files {
-		createFile(filepath.Join(projectPath, path), content, map[string]string{"ModuleName": moduleName})
+		createFile(filepath.Join(projectPath, path), content, map[string]string{
+			"ModuleName":  moduleName,
+			"ProjectName": projectName,
+			"Year":        "2023",
+			"Author":      "Raldes Krisnu",
+		})
 	}
 	// Create println file
 	fmt.Println("Project scaffold created successfully at", projectPath)
